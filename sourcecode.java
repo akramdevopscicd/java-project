8a19e339a4590635d83c25d6bb1a1936f60133de
root <root@ip-172-31-7-150.ap-south-1.compute.internal>
2024-09-10 14:56:54 +0000

    first commit

---
+++ sourcecode.java
@@ -0,0 +1,15 @@
+public class Sample {
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, World!");
+
+        // Loop through numbers 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+
+        // End of program
+        System.out.println("End of program.");
+    }
+}
+
